bacb0c56b710289893b9c60eeed4372e6285472a
nickaldwin <nicklemoncito98@gmail.com>
2023-10-19 13:48:38 +0800

    install express-async-hanlder

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,17 @@
+const asyncHandler = require("express-async-handler");
 const express = require("express");
 
 const registerUser = async(req, res) => {
-    if(!req.email.body){
-        res.status(400);
-        throw new Error ("Please add email")
-    }
-    res.send("Register User")
-}
+
+};
+
+
+
 
 
 module.exports = {
     registerUser,
 
 };
+
+
